8ad9b6dc5ebcc9caf623f9f48a542dd93267a045
foooomio <foooomio@gmail.com>
2020-02-02 22:22:00 +0900

    Add first implementation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # setup-rdflint
-GitHub Action to setup rdflint
+GitHub Action to setup [rdflint](https://github.com/imas/rdflint)
+
+## Usage
+
+```yml
+runs-on: ubuntu-latest
+steps:
+  - uses: actions/checkout@v2
+  - uses: actions/setup-java@v1
+    with:
+      java-version: 11
+  - uses: foooomio/setup-rdflint@v1
+  - run: rdflint -config rdflint-config.yml
+```
+
+## Inputs
+
+- `rdflint-version`: Optional. The version of rdflint to be installed. Exapmle: `0.1.2`. Defaults to `latest`.
+
+## License
+
+MIT License
---
+++ src/setup-rdflint.ts
@@ -0,0 +1,48 @@
+import fetch from 'node-fetch';
+import * as core from '@actions/core';
+import * as tc from '@actions/tool-cache';
+import * as fs from 'fs';
+import * as path from 'path';
+
+async function getLatestRdflintVersion(): Promise<string> {
+  const response = await fetch('https://jitpack.io/api/builds/com.github.imas/rdflint/latestOk');
+  const { version } = await response.json();
+
+  if (typeof version !== 'string') {
+    throw new Error('Failed to get the latest rdflint version');
+  }
+  return version;
+}
+
+async function installRdflint(version: string): Promise<string> {
+  const downloadUrl = `https://jitpack.io/com/github/imas/rdflint/${version}/rdflint-${version}.jar`;
+
+  const downloadPath = await tc.downloadTool(downloadUrl);
+  const cachePath = await tc.cacheFile(downloadPath, 'rdflint.jar', 'rdflint', version);
+
+  const executablePath = path.join(cachePath, 'rdflint');
+  fs.writeFileSync(executablePath, `#!/bin/sh\njava -jar ${cachePath}/rdflint.jar $@`);
+  fs.chmodSync(executablePath, 0o555);
+
+  core.addPath(cachePath);
+  return cachePath;
+}
+
+async function run(): Promise<void> {
+  try {
+    let version = core.getInput('rdflint-version');
+    if (!version || version === 'latest') {
+      version = await getLatestRdflintVersion();
+    }
+
+    let rdflintPath = tc.find('rdflint', version);
+    if (!rdflintPath) {
+      console.log('Installing rdflint ' + version);
+      rdflintPath = await installRdflint(version);
+    }
+  } catch (e) {
+    core.setFailed(e.message);
+  }
+}
+
+run();
